0269f469cf5faa97feb4cf4b375e6b1d6cc7d5e7
Emanuel Seidinger <dev@eseidinger.de>
2023-02-11 21:43:30 +0100

    implement genetic algorithm

---
+++ python_algos/ch05/chromosome.py
@@ -0,0 +1,24 @@
+from typing import TypeVar, Type
+from abc import ABC, abstractmethod
+
+T = TypeVar('T', bound='Chromosome')
+
+class Chromosome(ABC):
+
+    @abstractmethod
+    def fitness(self) -> float:
+        ...
+
+    @classmethod
+    @abstractmethod
+    def random_instance(cls: Type[T]) -> T:
+        ...
+
+    @abstractmethod
+    def crossover(self: T, other: T) -> tuple[T, T]:
+        ...
+
+    @abstractmethod
+    def mutate(self) -> None:
+        ...
+
