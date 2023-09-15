87f44d12482b27b70778708ff33321f09c320c40
Emanuel Seidinger <dev@eseidinger.de>
2023-09-15 09:03:18 +0200

    initial pyodide config

--- python-algos-ui/src/app/pyodide/pyodide.component.ts
+++ python-algos-ui/src/app/pyodide/pyodide.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { loadPyodide } from 'pyodide';
 
 @Component({
   selector: 'pau-pyodide',
   templateUrl: './pyodide.component.html',
   styleUrls: ['./pyodide.component.css']
 })
-export class PyodideComponent {
+export class PyodideComponent implements OnInit {
+
+  async ngOnInit() {
+    const pyodide = await loadPyodide();
+        console.log(pyodide.runPython(`
+            import sys
+            sys.version
+        `));
+        pyodide.runPython("print(1 + 2)");
+  }
 
 }
