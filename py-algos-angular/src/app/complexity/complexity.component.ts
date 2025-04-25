b7f0e07d4810b23ba90389b0173929c3e628af44
Emanuel Seidinger <dev@eseidinger.de>
2025-04-25 23:12:20 +0200

    add pyodide config

--- py-algos-angular/src/app/complexity/complexity.component.ts
+++ py-algos-angular/src/app/complexity/complexity.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { loadPyodide, PyodideInterface } from 'pyodide';
 
 @Component({
   selector: 'app-complexity',
@@ -7,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './complexity.component.scss'
 })
 export class ComplexityComponent {
+  pyodide!: PyodideInterface;
+
+  async ngOnInit() {
+    this.pyodide = await loadPyodide({ indexURL: '/' });
+    console.log(this.pyodide.runPython(`
+        import sys
+        sys.version
+      `));
+    await this.pyodide.loadPackage("https://files.pythonhosted.org/packages/43/e3/7d92a15f894aa0c9c4b49b8ee9ac9850d6e63b03c9c32c0367a13ae62209/mpmath-1.3.0-py3-none-any.whl");
+    await this.pyodide.loadPackage("https://files.pythonhosted.org/packages/99/ff/c87e0622b1dadea79d2fb0b25ade9ed98954c9033722eb707053d310d4f3/sympy-1.13.3-py3-none-any.whl");
+    await this.pyodide.loadPackage("https://test-files.pythonhosted.org/packages/ed/c8/b4107409c32d9d1aa3ffdcecd1e2bf579fb47d57dc38be3c0bf8768f41f2/py_algos_eseidinger-0.0.1-py3-none-any.whl");
+
+    const pkg = this.pyodide.pyimport('py_algos_eseidinger.complexity.varianttree');
+    console.log(pkg.main());
+  }
 
 }
--- py-algos-angular/src/app/maze/maze.component.ts
+++ py-algos-angular/src/app/maze/maze.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { loadPyodide, PyodideInterface } from 'pyodide';
 
 @Component({
   selector: 'app-maze',
@@ -6,6 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './maze.component.html',
   styleUrl: './maze.component.scss'
 })
-export class MazeComponent {
+export class MazeComponent implements OnInit {
+  pyodide!: PyodideInterface;
 
+  async ngOnInit() {
+    this.pyodide = await loadPyodide({ indexURL: '/' });
+    console.log(this.pyodide.runPython(`
+      import sys
+      sys.version
+    `));
+    await this.pyodide.runPythonAsync(`
+      from pyodide.http import pyfetch
+      response = await pyfetch('https://test-files.pythonhosted.org/packages/ed/c8/b4107409c32d9d1aa3ffdcecd1e2bf579fb47d57dc38be3c0bf8768f41f2/py_algos_eseidinger-0.0.1-py3-none-any.whl')
+      await response.unpack_archive()
+    `);
+    const pkg = this.pyodide.pyimport('py_algos_eseidinger.generic_search.maze');
+    console.log(pkg.main());
+  }
 }
