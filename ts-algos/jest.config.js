bf9af70059585327994b7532d4701ce57d9f6371
Emanuel Seidinger <dev@eseidinger.de>
2025-05-02 14:29:26 +0200

    switch to jasmine tests for ts lib

--- ts-algos/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  coverageDirectory: 'coverage',
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{ts,js}', '!src/index.ts'],
-};
